7e9ed07ff9549efcd9a45cc356e7453d10bd1ccc
Lelouch <flashgrinder92@gmail.com>
2020-07-10 13:02:47 +0400

    Написал скрипт смены слоганов на главном экране

--- app/js/scripts.min.js
+++ app/js/scripts.min.js
@@ -1,9 +1,20 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-
+    /* Particles */
     particlesJS.load('particles-js', 'js/particles.json', function() {
         console.log('callback - particles.js config loaded');
     });
 
+    /* Main-screen Slogan */
+    $('.main-screen__slogan:first').addClass('is-active');
+
+    setInterval(function(){
+        let $next = $('.main-screen__slogan.is-active')
+                    .removeClass('is-active')
+                    .next('.main-screen__slogan');
+
+        $next.length ? $next.addClass('is-active') : $('.main-screen__slogan:first').addClass('is-active');
+
+    }, 5000)
 
 });
