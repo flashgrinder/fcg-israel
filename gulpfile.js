5072684d36690dac60be0f7a179c8a1ab859e2f2
Lelouch <flashgrinder92@gmail.com>
2020-07-13 18:25:45 +0400

    Сверстал блок - Почему мы?, подправил стили в блоке Вам нужно

--- gulpfile.js
+++ gulpfile.js
@@ -64,7 +64,7 @@ function sassproc() {
 	.pipe(sass().on('error', sass.logError))
 	.pipe(concat('style.min.css'))
 	.pipe(gcmq())
-	.pipe(postcss([autoprefixer()]))
+	.pipe(postcss([autoprefixer({grid: 'autoplace'})]))
 	// .pipe(sourcemaps.write('.'))
 	.pipe(gulp.dest(source.app.css))
 	.pipe(browserSync.reload({ stream: true }));
